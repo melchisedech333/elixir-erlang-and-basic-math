9eafec7243ff8d0b0b1cddc47a4ac48a785e7a37
melchisedech333 <melchisedech333@gmail.com>
2022-10-31 15:11:02 -0300

    Updates

--- 0 - utils/division.ex
+++ 0 - utils/division.ex
@@ -1,9 +1,11 @@
 
+import Utils, only: [number_to_list: 1]
+
 defmodule Division do
     def divisible_by(value, number) do
         case number do
             2  -> rem(value, number) == 0
-            3  -> false
+            3  -> divisible_by_3(value)
             4  -> false
             5  -> false
             6  -> false
@@ -19,6 +21,19 @@ defmodule Division do
                 IO.puts("Division by #{number} not supported.")
                 false
         end
+    end
+
+    def divisible_by_3(value) do
+        # List  = utils:number_to_list(Value),
+        # Total = utils:sums_elements(List),
+        # Total rem 3 == 0.
+
+        list  = number_to_list(value)
+        total = 
+
+        IO.puts("list: #{ inspect(list) }")
+
+        false
     end
 end
 
--- 0 - utils/utils.ex
+++ 0 - utils/utils.ex
@@ -1,6 +1,24 @@
 
 defmodule Utils do
 
+    # 
+    # Access elements of a list, making it possible to change their 
+    # values, through an anonymous function. The function returns a 
+    # new list with the changed values. 
+    # 
+    #   mod = fn(x) -> x * 2 end
+    #   n = map_elements(mod, [ 1, 2, 3 ])
+    #
+
+    def map_elements(fun, [ first | rest ]) do
+        [ fun.(first) | map_elements(fun, rest) ]
+    end
+
+    def map_elements(fun, []) do
+        []
+    end
+
+
     #
     # Access elements of a list, through an anonymous function.
     # Allowing the passing of an external value, for it to be 
@@ -20,6 +38,25 @@ defmodule Utils do
 
     def access_elements_pvalue(fun, value, []) do 
         :ok
+    end
+
+
+    # 
+    # Converts a number to a list of numbers. The list returned by 
+    # the function is only composed of numbers, not characters from 
+    # the ASCII table. 
+    # 
+    #   list = number_to_list(333)
+    # 
+
+    def number_to_list(value) do
+        items = :erlang.integer_to_list(value)
+
+        adjust = fn(x) ->
+            x - 48 # ASCII table.
+        end
+
+        map_elements(adjust, items)
     end
 
 end
